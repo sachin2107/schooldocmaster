6018317ff3f52f8b23819fd4c7ca6cbba6fa8f29
sachin2107 <2107sachin@gmail.com>
2019-07-09 09:00:03 +0530

    Log4j added using properties and xml both way

---
+++ documentmaster/WebContent/WEB-INF/pages/login.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta charset="ISO-8859-1">
+	<title>Insert title here</title>
+</head>
+<body>
+	<div class="jumbotron">
+		<div><h2><spring:message code="loginwelcome"/></h2></div>
+		<button class="btn btn-info btn-lg">Submit</button>
+	</div>
+</body>
+</html>
--- documentmaster/src/main/java/com/school/docmaster/initcontroller/InitController.java
+++ documentmaster/src/main/java/com/school/docmaster/initcontroller/InitController.java
@@ -3,6 +3,9 @@ package com.school.docmaster.initcontroller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,9 +14,25 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/school")
 public class InitController {
 
+	protected final String CLASS_NAME = getClass().getName();
+//	If you want to use log4j.properties file then use Logger.getLogger(InitController.class) this line
+//	private final Logger logger = Logger.getLogger(InitController.class);
+	
+//	If you want to use log4j.xml file then use LogFactory.getLog
+	private final Log debugLogger = LogFactory.getLog("DEBUGGER"+this.CLASS_NAME);
 	@RequestMapping(value="/welcome")
 	public ModelAndView home(HttpServletRequest request, HttpServletResponse response)throws Exception
 	{
 		return new ModelAndView("welcome");
+	}
+	
+	@RequestMapping(value="/login")
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response)throws Exception
+	{	
+//		if(logger.isDebugEnabled())
+//			logger.debug("inside login method...");
+		if(debugLogger.isDebugEnabled())
+			debugLogger.debug("in hsbc logger...");
+		return new ModelAndView("login");
 	}
 }
